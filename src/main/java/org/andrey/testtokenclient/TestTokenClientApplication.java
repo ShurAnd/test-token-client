af61a42ed2550ce29883de9aeb400b29484556c6
AAShurmanov <AAShurmanov@MZ-WD-31000104.mbp>
2021-04-27 15:50:02 +0300

    init commit

---
+++ src/main/java/org/andrey/testtokenclient/TestTokenClientApplication.java
@@ -0,0 +1,13 @@
+package org.andrey.testtokenclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTokenClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTokenClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/andrey/testtokenclient/controller/ArticlesController.java
@@ -0,0 +1,32 @@
+package org.andrey.testtokenclient.controller;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+public class ArticlesController {
+
+	private WebClient webClient;
+	
+	@Autowired
+	public ArticlesController(WebClient webClient) {
+		this.webClient = webClient;
+	}
+	
+	@GetMapping("/articles")
+	public String[] getArticles(@RegisteredOAuth2AuthorizedClient("articles-client-authorization-code") OAuth2AuthorizedClient authorizedClient) {
+		return this.webClient
+		          .get()
+		          .uri("http://localhost:9090/articles")
+		          .attributes(oauth2AuthorizedClient(authorizedClient))
+		          .retrieve()
+		          .bodyToMono(String[].class)
+		          .block();
+	}
+}
